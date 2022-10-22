d513ac2208bb6373ed81b6118f1dcd8c5033ffe8
Robot <lbh9311@sina.com>
2022-10-22 19:54:43 +0800

    feat: init

--- prisma/migrations/20221020143203_/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "TrackEvmAccount" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "chainId" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "type" INTEGER NOT NULL
-);
